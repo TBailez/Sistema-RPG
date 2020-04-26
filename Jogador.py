f7f4fdaa268f14e889bdbbd9682da0c203906e6f
TBailez <52189272+TBailez@users.noreply.github.com>
2020-04-26 19:15:08 -0300

    Add files via upload

--- Jogador.py
+++ Jogador.py
@@ -1,5 +1,16 @@
-
-nome = input('Qual seu nome? \n')
+nome = input('Qual é o seu nome? \n')
+info = input('Você quer adicionar informações adicionais? \n')
+if info == 'Sim' or info == 'SIM' or info == 'sim' :
+    idade = input('Quantos anos você tem? \n')
+    cabelo = input('Qual a cor de seu cabelo? \n')
+    altura = input('Qual a sua altura? \n')
+    peso = input('Quanto você pesa? \n')
+    alinhamento = input('Qual o seu alinhamento? \n')
+    vestimenta = input('Descreva sua vestimenta \n')
+elif info == 'Não' or info == 'não' or info == 'NÃO' or info == 'NAO' or info == 'Nao' or info == 'nao' :
+        print("Ótima escolha")
+else:
+        print("As opções eram sim ou não, mas tudo bem, vou considerar um não!")
 FOR = input('Quanto de força você tem? \n')
 INT = input('Quanto de inteligência você tem? \n')
 RES = input('Quanto de resistência você tem? \n')
@@ -15,25 +26,29 @@ int_RES = int(RES)
 int_ING = int(ING)
 int_VEL = int(VEL)
 RAÇA = input('Qual a sua Raça? \n')
-if RAÇA == 'Humano' :
+if RAÇA == 'Humano' or RAÇA =='humano' or RAÇA == 'HUMANO' :
     int_FOR += 1
     int_RES += 1
     int_VEL += 1
     
-elif RAÇA == 'Elfo':
+elif RAÇA == 'Elfo' or RAÇA == 'elfo' or RAÇA =='ELFO':
     int_INT += 2
     int_VEL += 1
-elif RAÇA == 'Orc':
+elif RAÇA == 'Orc' or RAÇA == 'orc' or RAÇA == 'ORC':
     int_FOR += 2
     int_RES += 2
     int_CHP += 1
     int_INT -= 1
     int_VEL -= 1
     
+CLASSE = input('Qual a sua classe \n')
+
 
 HP = int_CHP*15
 MN = int_CMN*15
 print('Os status de',(nome),'são:')
+print('Raça:',(RAÇA))
+print('Classe',(CLASSE))
 print('HP:',(HP))
 print('MANA:',(MN))
 print('Força:',(int_FOR))
