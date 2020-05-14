17b3f79cd0bcd080ee51e5c9f27911f8fa929fd5
Tomás Bailez <tomasbailez11@gmail.com>
2020-05-13 22:07:10 -0300

    ajustando restaurar

--- Beta/scripts/funcoes/restore.py
+++ Beta/scripts/funcoes/restore.py
@@ -12,17 +12,15 @@ def restaurar():
         if perso in nomes or perso in npcs:
          status=input('HP ou Mana?\n')
          if status=='hp':
-            print('Seu hp agora é de', nomes[perso]['hp'])
+            print('Seu hp era de', nomes[perso]['hp'])
             reshp=(nomes[perso]['chp'])
             hp=int(reshp*15)
-            print(hp)
             nomes[perso]['hp'] = hp
             print('Seu hp foi restaurado para',nomes[perso]['hp'])
          elif status=='mana':
-            print('Sua mana agora é de',(nomes[perso]['mana']))
+            print('Sua mana era de',(nomes[perso]['mana']))
             resmn=(nomes[perso]['cmana'])
             mana=int(resmn*15)
-            print(mana)
             nomes[perso]['mana'] = mana
             print('Sua mana foi restaurada para',nomes[perso]['mana'])
         else: print('Esse nome não existe')
