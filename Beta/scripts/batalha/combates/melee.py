071271a571f477d48feaf3d06c5d48ce407e5736
Tomás Bailez <tomasbailez11@gmail.com>
2020-05-14 19:59:29 -0300

    renovando o sistema de batalha com armas e armaduras '

--- Beta/scripts/batalha/combates/melee.py
+++ Beta/scripts/batalha/combates/melee.py
@@ -6,6 +6,12 @@ with open('Beta/data/nomes.json') as f:
 with open('Beta/data/npcs.json') as g:
     npcs=json.load(g)
 
+with open('Beta/data/inventario/armas.json') as h:
+    armas=json.load(h)
+
+with open('Beta/data/inventario/armadura.json') as i:
+    armadura=json.load(i)
+
 def melee(natk,q,combatentes):
     print("Vez de",natk,"atacar",q,"vez(es)")
     quanti=0
@@ -20,14 +26,23 @@ def melee(natk,q,combatentes):
         if natk in nomes: non=nomes.get(natk)
         else: non=npcs.get(natk)
         f=int(non.get('forca'))
-        boar=int(non.get('inventario').get('arma'))
+        #estou marcando onde mudei para ficar facil de notar
+        ar=non.get('inventario').get('arma')
+        if ar in armas: boar=int(armas.get(ar).get('dme'))
+        #fim
         quo_atk=f+boar+dd
         quo_vel_atk=dd+int(non.get('velocidade'))
         if nomedef in nomes: non=nomes.get(nomedef)
         else: non=npcs.get(nomedef)
         f_def=int(non.get('forca'))
-        boar_def=int(non.get('inventario').get('arma'))
-        quo_def=int(non.get('resistencia'))+int(non.get('inventario').get('armadura'))
+        #inicio
+        ar_def=non.get('inventario').get('arma')
+        if ar_def in armas: boar_def=int(armas.get(ar).get('dme'))
+        #fim/inicio
+        armad_def=non.get('inventario').get('armadura')
+        if armad_def in armadura: armadura_def=int(armadura.get(armad_def).get('rme'))
+        quo_def=int(non.get('resistencia'))+armadura_def
+        #
         dano=quo_atk-quo_def
         desvio=quo_vel_atk-int(non.get('velocidade'))
         parry=quo_atk-(f_def+boar_def)
