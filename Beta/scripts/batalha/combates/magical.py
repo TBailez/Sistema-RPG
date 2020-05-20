821288084887853f43629a0775fdabc479d9adca
Tomás Bailez <tomasbailez11@gmail.com>
2020-05-20 16:14:58 -0300

    consertei uns trens nos criadores e vou tentar resolver o problema do healing

--- Beta/scripts/batalha/combates/magical.py
+++ Beta/scripts/batalha/combates/magical.py
@@ -60,7 +60,7 @@ def magical(natk,combatentes):
     if (d1+non.get('inteligencia'))>=spell.get('dificuldade') or d1==0:
         if spell.get('Type')=='heal':
             while True:
-                qual=input('Qual nome do personagem que você deseja helar?')
+                qual=input('Qual nome do personagem que você deseja healar?')
                 if qual in combatentes: break
                 else: print('Esse personagem não está no combate')
             if combatentes.get(qual).get('hp')+spell.get('dano')>combatentes.get(qual).get('chp')*15: combatentes[qual]['hp']=combatentes.get(qual).get('chp')*15
--- Beta/scripts/funcoes/creators/magiccreator.py
+++ Beta/scripts/funcoes/creators/magiccreator.py
@@ -19,7 +19,7 @@ def mcreator():
     v=int(input('velocidade?\n'))
     c=[]
     while True:
-        cl=input('Qual classe?')
+        cl=input('Qual classe?\n')
         if cl=='all' or cl=='a':
             c.append("clerigo")
             c.append("druida")
@@ -30,23 +30,25 @@ def mcreator():
             break
         if cl in classes:
             c.append(cl)
-            con=input('Dejesa adicionar outra classe?')
+            con=input('Dejesa adicionar outra classe?\n')
             if con=='s': pass
             else: break
         elif c=='l':
             for cla in classes:
                 print(cla)
         else: print('burro essa classe n exite')
+    t=input('Qual o tipo da magia, dano ou heal?\n')
     dados={
         'requisito': r,
         'dificuldade': d,
         'mana': m,
         'dano': dano,
         'velocidade': v,
-        'classes': c
+        'classes': c,
+        'Type': t
     }
     magias.update({name:dados})
-    c=input('dejeja criar outra magia?')
+    c=input('dejeja criar outra magia?\n')
     with open('Beta/data/magias.json','w') as f:
         json.dump(magias,f)
     if c=='s': pass
