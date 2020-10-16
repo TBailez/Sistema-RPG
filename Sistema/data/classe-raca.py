792c623b594839fbe7aef016bc43fe81f0480246
Jplup <jpulup@gmail.com>
2020-10-16 03:21:54 -0300

    fiz umas coisas ai

--- Sistema/data/classe-raca.py
+++ Sistema/data/classe-raca.py
@@ -1,3 +1,5 @@
+import pickle
+
 def cria():
     lista_de_racas=['humano','elfo','anao','orc','tiefling','deva','qunari','draconato']
     while True:
@@ -15,24 +17,50 @@ def cria():
         else: print('Não existe essa opção\n Lista de classes:\n   ',lista_de_classes)
     return [ra,cla]
 
-racla=cria()
-ra=racla[0]
-cla=racla[1]
+escolha=input('Deseja cria um personagem ou printar ou usar use: ')
+if escolha=='c':
+    racla=cria()
+    ra=racla[0]
+    cla=racla[1]
+else:
+    ra=1
+    cla=1
+
 
 #--------------------------------------------------------------------------------
+class nothing: pass
+#--------------------------------------------------------------------------------
+
+def teste1():
+    print('tre')
 
 class fighter:
-    def __init__(self): pass
+    def __init__(self):
+        self.stamina=20
+        self.func=[teste1.__name__]
 
 class wizard:
-    def __init__(self): pass
+    def __init__(self):
+        self.quantidade_de_spells=10
 
 class assassin:
-    def __init__(self): pass
+    def __init__(self):
+        self.critical_chance=30
 
-class racas(fighter if cla=='fighter' else wizard if cla=='wizard' else assassin):
+class racas(fighter if cla=='fighter' else wizard if cla=='wizard' else assassin if cla=='assassin' else nothing):
     def init_racas(self):
         super().__init__()
+
+    def use(self):
+        try: t=self.func
+        except: self.func=[]
+        print(self.name,'tem essas funções:')
+        for f in self.func: print('  ',f)
+        while True:
+            qual=input('Qual sera usada?')
+            if qual in self.func: globals()[qual]()
+            elif qual=='n': break
+            else: print('Não existe essa opção')
 
 #--------------------------------------------------------------------------------
 
@@ -83,7 +111,7 @@ class draconato:
         self.Chp=1
         self.intransigencia=2
 
-class classes(humano if ra=='humano' else elfo if ra=='elfo' else anao if ra=='anao' else orc if ra=='orc' else tiefling if ra=='tiefling' else deva if ra=='deva' else qunari if ra=='qunari' else draconato):
+class classes(humano if ra=='humano' else elfo if ra=='elfo' else anao if ra=='anao' else orc if ra=='orc' else tiefling if ra=='tiefling' else deva if ra=='deva' else qunari if ra=='qunari' else draconato if ra=='draconato' else nothing):
     def init_classe(self):
         self.forca=0
         self.inteligencia=0
@@ -101,10 +129,31 @@ class personagem(classes,racas):
     def __init__(self):
         super().init_classe()
         super().init_racas()
+        self.name=input('Qual o nome do personagem: ')
 
     def printar(self):
         print('forca:',self.forca,'inteligencia:',self.inteligencia,'Chp:',self.Chp,'Cmana:',self.Cmana,'pontos_lvl:',self.pontos_lvl)
         print('resistencia:',self.resistencia,'intransigencia:',self.intransigencia,'velocidade:',self.velocidade)
 
-player=personagem()
-player.printar()
+try:
+    with open('Sistema/data/players.pickle','rb') as f:
+        P=pickle.load(f)
+except: P=[]
+
+if escolha=='c':
+    player=personagem()
+    P.append(player)
+    with open('Sistema/data/players.pickle','wb') as f:
+        pickle.dump(P,f)
+
+elif escolha=='p':
+    for p in P:
+        lista={}
+        d=dir(p)
+        for atr in d:
+            splitd=atr.split(sep='__')
+            if len(splitd)==1:
+                lista[atr]=getattr(p,atr)
+        print(p.name,':\n  ',lista)
+else:
+    P[0].use()
